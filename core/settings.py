f7586160d4b586bc7320a2a0f3f02d5692c11287
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-05-02 11:04:50 +0500

    ..

--- core/settings.py
+++ core/settings.py
@@ -19,6 +19,10 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+      #...
+    "admin_interface",
+    "colorfield",
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,9 +109,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT=os.path.join(BASE_DIR ,'static')
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # MEDIA_ROOT=[BASE_DIR/'media',]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
